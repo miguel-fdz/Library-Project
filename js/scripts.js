a82830dcda00171ab0bc71c42dfb4394792f882a
Miguel Fernandez <miguelf.olalla@gmail.com>
2020-04-19 11:11:06 -0300

    added to js, css,html files

--- js/scripts.js
+++ js/scripts.js
@@ -11,24 +11,27 @@ const buttonFunctionalities = {
     const readStatus = document.getElementById("rating-question");
     (readStatus.style.display === "block") ? readStatus.style.display = "none" : readStatus.style.display = "block";
 },
-  "newBook": () => document.getElementById("form__modal").style.display = "block",
+  "newBook": () => document.querySelector(".form__modal").style.display = "block",
   "addBook": () => {
     myLibrary.push(new Book());
     buttonFunctionalities.cancelForm();
   },
   "cancelForm": () => {
-    document.getElementById("form__modal").style.display = "none";
+    document.querySelector(".form__modal").style.display = "none";
     document.querySelectorAll(".form__question--input").forEach(e => e.value = "");
   },
 }
 
 const bookValue = (x) => document.getElementById(`${x}`).value;
 
-function Book() {
-  this.title = bookValue("titleName");
-  this.author = bookValue("authorName");
-  this.pages = bookValue("pageAmount");
-  this.read = bookValue("readStatus");
-  this.rating = bookValue("rating");
-  this.toggleRead = () => {};
+class Book {
+  constructor(title, author, pageCount, read) {
+    this.title = bookValue("titleName");
+    this.author = bookValue("authorName");
+    this.pages = bookValue("pageCount");
+    (bookValue("readStatus") === "on") ? this.read = true : this.read = false;
+    this.rating = bookValue("rating");
+    this.toggleRead = () => {};
+    this.deleteBook = () => {};
+  }
 }
