edcc7b250db4a77b5341ff7efb24b98cd2201b3c
Josse Pickard <jossepickard@gmail.com>
2020-04-19 18:55:25 +0100

    edited table functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 document.querySelectorAll(".form__button").forEach(button => {
   button.addEventListener("click", (e) => {
@@ -15,6 +15,7 @@ const buttonFunctionalities = {
   "addBook": () => {
     myLibrary.push(new Book());
     buttonFunctionalities.cancelForm();
+    render(myLibrary[-1], "table");
   },
   "cancelForm": () => {
     document.querySelector(".form__modal").style.display = "none";
@@ -22,16 +23,52 @@ const buttonFunctionalities = {
   },
 }
 
-const bookValue = (x) => document.getElementById(`${x}`).value;
+const bookValue = x => document.getElementById(`${x}`).value;
 
 class Book {
-  constructor(title, author, pageCount, read) {
+  constructor() {
     this.title = bookValue("titleName");
     this.author = bookValue("authorName");
     this.pages = bookValue("pageCount");
-    (bookValue("readStatus") === "on") ? this.read = true : this.read = false;
+    this.read = document.getElementById("readStatus").checked;
     this.rating = bookValue("rating");
     this.toggleRead = () => {};
+    this.changeRating = () => {};
     this.deleteBook = () => {};
   }
 }
+
+document.getElementById("displayList").addEventListener("change", (e) => {
+  myLibrary.forEach(book => render(e.target.id, "table"))
+})
+
+const render = (book, type) => {
+  if (type === "table") {
+    const table = document.getElementById("bookshelf-table");
+    const newRow = document.createElement("div");
+    newRow.classList.add("bookshelf__body--row"); 
+    
+    const titleCell = document.createElement("div");
+    titleCell.classList.add("bookshelf__body--row-cell");
+    titleCell.innerHTML = book.title;
+    const authorCell = document.createElement("div");
+    authorCell.classList.add("bookshelf__body--row-cell");
+    authorCell.innerHTML = book.author;
+    const pageCell = document.createElement("div");
+    pageCell.classList.add("bookshelf__body--row-cell");
+    pageCell.innerHTML = book.pages;
+    const readCell = document.createElement("div");
+    readCell.classList.add("bookshelf__body--row-cell");
+    readCell.innerHTML = book.read;
+    const ratingCell = document.createElement("div");
+    ratingCell.classList.add("bookshelf__body--row-cell");
+    ratingCell.innerHTML = book.rating;
+    
+    newRow.appendChild(titleCell);
+    newRow.appendChild(authorCell);
+    newRow.appendChild(pageCell);
+    newRow.appendChild(readCell);
+    newRow.appendChild(ratingCell);
+    table.appendChild(newRow);
+  }
+};
