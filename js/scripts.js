4422e0d847698d2b350ea46fcc4328b4126f6bb7
Miguel Fernandez <miguelf.olalla@gmail.com>
2020-04-19 09:10:16 -0300

    added to JS file and tweaked some visuals

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 document.querySelectorAll(".form__button").forEach(button => {
   button.addEventListener("click", (e) => {
     buttonFunctionalities[e.target.id]();
@@ -5,14 +7,28 @@ document.querySelectorAll(".form__button").forEach(button => {
 })
 
 const buttonFunctionalities = {
-  "read": () => {
+  "readStatus": () => {
     const readStatus = document.getElementById("rating-question");
     (readStatus.style.display === "block") ? readStatus.style.display = "none" : readStatus.style.display = "block";
 },
   "newBook": () => document.getElementById("form__modal").style.display = "block",
-  "addBook": () => alert("This button has no functionalities yet."),
-  "cancelBook": () => {
+  "addBook": () => {
+    myLibrary.push(new Book());
+    buttonFunctionalities.cancelForm();
+  },
+  "cancelForm": () => {
     document.getElementById("form__modal").style.display = "none";
     document.querySelectorAll(".form__question--input").forEach(e => e.value = "");
   },
+}
+
+const bookValue = (x) => document.getElementById(`${x}`).value;
+
+function Book() {
+  this.title = bookValue("titleName");
+  this.author = bookValue("authorName");
+  this.pages = bookValue("pageAmount");
+  this.read = bookValue("readStatus");
+  this.rating = bookValue("rating");
+  this.toggleRead = () => {};
 }
