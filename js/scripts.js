ab63f20129659c24f5a4905bee139cf898bcd76d
Miguel Fernandez <miguelf.olalla@gmail.com>
2020-04-20 21:57:39 -0300

    added remove buttons functionality and restructured rendering process slightly

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
-const myLibrary = [];
+const myLibrary = [{
+  title: "the hobbit",
+  author: "tolkein",
+  pages: "123 pages",
+  read: true,
+  rating: "***",
+}];
+const librarySettings = {
+  display: "table",
+  sort: "title",
+}
 
+/* Saves index of each book withing corresponding object. */
+const setIndex = () => {
+  for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary[i].index = i;
+  }
+}
+
+/* Adds functionality for all books in library */
 document.querySelectorAll(".form__button").forEach(button => {
   button.addEventListener("click", (e) => {
     buttonFunctionalities[e.target.id]();
@@ -8,19 +26,46 @@ document.querySelectorAll(".form__button").forEach(button => {
 
 const buttonFunctionalities = {
   "readStatus": () => {
-    const readStatus = document.getElementById("rating-question");
+    const readStatus = document.getElementById("ratingInput");
     (readStatus.style.display === "block") ? readStatus.style.display = "none" : readStatus.style.display = "block";
 },
   "newBook": () => document.querySelector(".form__modal").style.display = "block",
   "addBook": () => {
     myLibrary.push(new Book());
+    setIndex();
+    removeBooks();
+    myLibrary.forEach(book => render(book, librarySettings.display));
     buttonFunctionalities.cancelForm();
-    render(myLibrary[-1], "table");
   },
   "cancelForm": () => {
     document.querySelector(".form__modal").style.display = "none";
     document.querySelectorAll(".form__question--input").forEach(e => e.value = "");
   },
+  "editBook": () => {
+    const index = findIndex(event.srcElement.parentNode); 
+    console.log(index);
+  },
+  "removeBook": () => {
+    const index = findIndex(event.srcElement.parentNode); 
+    myLibrary.splice(index, 1);
+    setIndex();
+    removeBooks();
+    myLibrary.forEach(book => render(book, librarySettings.display))
+  },
+}
+
+/* Finds index of type node within parent */
+const findIndex = (child) => {
+  const parent = child.parentNode; 
+  return Array.prototype.indexOf.call(parent.children, child);
+}
+
+ /* Resets table for re-rendering books*/
+const removeBooks = () => {
+  const tableBody = document.getElementById("body");
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.lastChild);
+  }
 }
 
 const bookValue = x => document.getElementById(`${x}`).value;
@@ -29,7 +74,7 @@ class Book {
   constructor() {
     this.title = bookValue("titleName");
     this.author = bookValue("authorName");
-    this.pages = bookValue("pageCount");
+    this.pages = `${bookValue("pageCount")} pages`;
     this.read = document.getElementById("readStatus").checked;
     this.rating = bookValue("rating");
     this.toggleRead = () => {};
@@ -38,37 +83,41 @@ class Book {
   }
 }
 
+/* Adds display change functionality */
 document.getElementById("displayList").addEventListener("change", (e) => {
-  myLibrary.forEach(book => render(e.target.id, "table"))
+  const currentDisplayType = document.getElementById(`library-${librarySettings.display}`)
+  currentDisplayType.style.display = "none";
+  const newDisplayType = document.getElementById(`library-${e.target.value}`)
+  newDisplayType.style.display = "";
+  librarySettings.display = e.target.value;
 })
 
-const render = (book, type) => {
-  if (type === "table") {
-    const table = document.getElementById("bookshelf-table");
-    const newRow = document.createElement("div");
-    newRow.classList.add("bookshelf__body--row"); 
-    
-    const titleCell = document.createElement("div");
-    titleCell.classList.add("bookshelf__body--row-cell");
-    titleCell.innerHTML = book.title;
-    const authorCell = document.createElement("div");
-    authorCell.classList.add("bookshelf__body--row-cell");
-    authorCell.innerHTML = book.author;
-    const pageCell = document.createElement("div");
-    pageCell.classList.add("bookshelf__body--row-cell");
-    pageCell.innerHTML = book.pages;
-    const readCell = document.createElement("div");
-    readCell.classList.add("bookshelf__body--row-cell");
-    readCell.innerHTML = book.read;
-    const ratingCell = document.createElement("div");
-    ratingCell.classList.add("bookshelf__body--row-cell");
-    ratingCell.innerHTML = book.rating;
-    
-    newRow.appendChild(titleCell);
-    newRow.appendChild(authorCell);
-    newRow.appendChild(pageCell);
-    newRow.appendChild(readCell);
-    newRow.appendChild(ratingCell);
-    table.appendChild(newRow);
+/* Renders one book according to display selection. */
+const render = (book) => {
+  if (librarySettings.display === "table") {
+    const container = document.getElementById("body");
+    const createRow = document.createElement("div");
+    createRow.classList.add("library__body--row");
+    const element = [book.title, book.author, book.pages, book.read, book.rating, "edit", "remove"];
+    for (let i = 0; i < 5; i++) {
+      const Cell = document.createElement("div");
+      Cell.classList.add("library__body--row-cell");
+      Cell.innerHTML = element[i];
+      createRow.appendChild(Cell);
+    }
+    for (let i = 5; i < 7; i++) {
+      const Button = document.createElement("button");
+      Button.classList.add("form__button");
+      Button.setAttribute("id", `${element[i]}Book`),
+      Button.innerHTML = element[i];
+      Button.addEventListener("click", () => buttonFunctionalities[Button.id]());
+      createRow.appendChild(Button);
+    }
+    container.appendChild(createRow);
   }
-};
+  else {
+    alert("this doesn't work yet!");
+  } 
+}
+
+myLibrary.forEach(book => render(book, librarySettings.display))
